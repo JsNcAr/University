ad1dcc2a940b46f127b969cffca35249ebb5ef21
Nicolas Arias <jn.arias@uniandes.edu.co>
2020-02-08 17:46:41 -0500

    1. Se separaron los metodos generalizables del archivo especifico 2. Se eliminaron partes redundantes e innecesarias del codigo 3. Se separaron los datos del archivo principal

---
+++ Herramientas/MetodosBasicos.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # <font color = 'DodgerBlue'> Metodos basicos para el trabajo en fisica computacional </font>
+
+# ## <font color = #585858> Este programa contiene los siguiente metodos basicos para el analisis fisico:
+# 
+# -   Cuadratica (x,a,b,c): dados los parametros retorna una funcion cuadratica
+# -   
+# -   
+# 
+# </font>
+
+# In[1]:
+
+
+def Cuadratica(x, a, b, c):
+   return (a*(x**2))+ b*x + c
+
