4876830e2e56492ea2d1db3bc4eb8ad946f25e3f
Jason Nicolas Arias Gomez <UANDES\jn.arias@fisicaey110b09l.ad.uniandes.edu.co>
2020-02-10 20:12:18 -0500

    .

---
+++ Moderna/labo2/Datos.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[4]:
+
+
+E11 = np.array( [0.5, 1, 1.4, 1.74] )
+E12 = np.array([0.5, 0.9, 1.3, 1.6])
+E13 = np.array([0.5, 0.8, 1.08, 1.33])
+E14 = np.array([0.3, 0.4, 0.53, 0.66])
+E15 = np.array([0.2, 0.32, 0.47, 0.67, 0.87])
+
+I4 = np.linspace( 1, 4, num = 4 )
+I5 = np.linspace( 1, 5, num = 5 )
+
+
+# In[3]:
+
+
+
+
