e9ceb08e6a801bfc1f041f010835da9464e68894
Nicolas Arias <jn.arias@uniandes.edu.co>
2020-02-25 13:27:40 -0500

    Se agrgaron los datos del laboratorio 4

---
+++ Moderna/Labo4/Datos.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # <font color='DodgerBlue'>Datos laboratorio 4 de fisica moderna</font> 
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+y = pd.read_csv('amarillo.csv') 
+b = pd.read_csv('azul.csv')
+r = pd.read_csv('rojo.csv')
+g = pd.read_csv('verde.csv')
+
