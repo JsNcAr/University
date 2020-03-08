c5ed30303e70a0525156f7c3eda0273fe5426082
Nicolas Arias <jn.arias@uniandes.edu.co>
2020-03-08 14:51:28 -0400

    Finalizado labo 5

--- Moderna/Labo5/Datos.py
+++ Moderna/Labo5/Datos.py
@@ -13,5 +13,7 @@ import pandas as pd
 deu = pd.read_csv('deuprueba.csv', index_col= 'px')
 h = pd.read_csv('hidro.csv', index_col= 'px')
 ne = pd.read_csv('ne.csv', index_col = 'px')
+na = pd.read_csv('na.csv', index_col = 'px')
+hg = pd.read_csv('hg.csv', index_col = 'px')
 cd = 1167 
 ch = 1227 
