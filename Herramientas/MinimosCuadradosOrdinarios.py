c3b38185e5e93a2357495fc612bf6c0f5ddf07d4
Jason Nicolas Arias <jn.arias@uniandes.edu.co>
2022-07-23 18:49:26 -0500

    Improved code readeablity

--- Herramientas/MinimosCuadradosOrdinarios.py
+++ Herramientas/MinimosCuadradosOrdinarios.py
@@ -1,15 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
+# #  Metodo para calcular minimos cuadrados ordinarios
 
-# # <font color = 'DodgerBlue'> Metodo para calcular minimos cuadrados ordinarios </font>
-
-# ## <font color = #585858> Este programa contiene metodos que calculan el valor de los parametros optimos para reducir al minimo el error cuadratico en un ajuste lineal de 2 arrays </font>
-
-# _________________
-
-#  <font color ='gray'> se importa numpy para poder trabajar comodamente con matrices para soluciar el problema por el metodo vectorial </font>
-
-# In[1]:
+# ## Este programa contiene metodos que calculan el valor de los parametros optimos para reducir al minimo el error cuadratico en un ajuste lineal de 2 arrays
 
 
 import numpy as np
@@ -18,45 +9,45 @@ import numpy as np
 # #  Formula para hallar $\beta $ que minimiza el error cuadratico medio (MCO)
 # $ \beta = (X^{T}X)^{-1} X^{T} Y$
 
-# In[2]:
+# El metodo recibe 2 arrays y nos da los parametros para la regresion lineal que minimiza el error cuadratico
+def MinCua(a, b):
 
-
-#El metodo recibe 2 arrays y nos da los parametros para la regresion lineal que minimiza el error cuadratico
-def MinCua(a,b):
-    
-    #Se convierten en arrays de numpy para poder usar los metodos de dicha libreria
+    # Se convierten en arrays de numpy para poder usar los metodos de dicha libreria
     xt = np.array(a)
     Y = np.array(b)
 
-    #se añade columna de unos(1) para representar el termino independiente
-    C1 =  np.ones(len(xt))
-    
+    # se añade columna de unos(1) para representar el termino independiente
+    C1 = np.ones(len(xt))
+
     """" 
     Se transpone X debido a que la matriz de parametros beta resultante, tiene la forma de 1 fila y 2 coumnas
     por tanto no esta en la forma vectorial sino en la vectorial transpuesta 
     """
-    
+
     X = np.array([C1, xt]).T
 
     # @ = Multiplicacion Matricial
     # X.T = np.transpose(x) = X transpuesta
     B = np.linalg.inv(X.T @ X) @ X.T @ Y
 
-    #Esta operacion nos da como resultado una lista con 2 parametros, el primero representa la variable independiente
+    # Esta operacion nos da como resultado una lista con 2 parametros, el primero representa la variable independiente
     # y el segundo la pendiente optima para minimizar el error cuadratico, esta formula se demostro en algebra lineal
     #b = B[0]
     #m = B[1]
-    return [B[0],B[1]]
+    return [B[0], B[1]]
 
 
-#  <font color ='gray'> Finalmente el metodo nos retorna un array con 5 parametros, el primero representa el intercepto de la recta solucion, el segundo la pendiente, el tercero es el array dado como parametro 'a' (representando la variable independiente) convertido a numpy, el cuarto es este mismo array pero en forma de matriz con una columna de 1's representando el parametro independiente, y el quinto representa el parametro 'b' (variable dependiente) convertido a numpy  </font>
+"""
+Finalmente el metodo nos retorna un array con 5 parametros, el primero representa el intercepto de la recta solucion, 
+el segundo la pendiente, el tercero es el array dado como parametro 'a' (representando la variable independiente) 
+convertido a numpy, el cuarto es este mismo array pero en forma de matriz con una columna de 1's representando el parametro 
+independiente, y el quinto representa el parametro 'b' (variable dependiente) convertido a numpy.
+"""
 
-# In[3]:
 
+def MinCua2(y, x1, dim=2, x2=[0], x3=[0], x4=[0], x5=[0]):
 
-def MinCua2(y,x1, dim = 2, x2=[0], x3=[0], x4=[0], x5=[0]):
-    
-    #Se convierten en arrays de numpy para poder usar los metodos de dicha libreria
+    # Se convierten en arrays de numpy para poder usar los metodos de dicha libreria
     Y = np.array(y)
     x1t = np.array(x1)
     x2t = np.array(x2)
@@ -64,19 +55,19 @@ def MinCua2(y,x1, dim = 2, x2=[0], x3=[0], x4=[0], x5=[0]):
     x4t = np.array(x4)
     x5t = np.array(x5)
 
-    #se añade columna de unos(1) para representar el termino independiente
-    C1 =  np.ones(len(x1t))
-    
+    # se añade columna de unos(1) para representar el termino independiente
+    C1 = np.ones(len(x1t))
+
     """" 
     Se transpone X debido a que la matriz de parametros beta resultante, tiene la forma de 1 fila y 2 coumnas
     por tanto no esta en la forma vectorial sino en la vectorial transpuesta 
     """
-    if dim ==1:
+    if dim == 1:
         X = np.array([x1t]).T
     elif dim == 2:
         X = np.array([C1, x1t]).T
     elif dim == 3:
-        X = np.array([C1, x1t,x2t]).T
+        X = np.array([C1, x1t, x2t]).T
     elif dim == 4:
         X = np.array([C1, x1t, x2t, x3t]).T
     elif dim == 5:
@@ -89,10 +80,9 @@ def MinCua2(y,x1, dim = 2, x2=[0], x3=[0], x4=[0], x5=[0]):
     # X.T = np.transpose(x) = X transpuesta
     B = np.linalg.inv(X.T @ X) @ X.T @ Y
 
-    #Esta operacion nos da como resultado una lista con 2 parametros, el primero representa la variable independiente
+    # Esta operacion nos da como resultado una lista con 2 parametros, el primero representa la variable independiente
     # y el segundo la pendiente optima para minimizar el error cuadratico, esta formula se demostro en algebra lineal
     #b = B[0]
     #m_x1 = B[1]
-    #m_x2 = B[2] ...
+    # m_x2 = B[2] ...
     return [B]
-
--- Herramientas/recursividad.py
+++ Herramientas/recursividad.py
@@ -1,56 +1,47 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
-"primera parte funcion factorial"
+# primera parte funcion factorial
 
 count = 0
 
 while count <= 5:
-    
-    x = int(input("intruducir un numero natural"))
-    
-    def factorial(x):
-        
-        if x < 2:
-            
-            return 1
-        
-        else:
-        
-            return x*factorial(x-1)
-    
-    print("el factorial de", x , "es", factorial(x))
-    
-    count += 1 
 
-"segunda parte fibbonachi"
+    x = int(input("intruducir un numero natural"))
+
+    def factorial(x):
+
+        if x < 2:
+
+            return 1
+
+        else:
+
+            return x*factorial(x-1)
+
+    print("el factorial de", x, "es", factorial(x))
+
+    count += 1
+
+# segunda parte fibbonachi
 
 count2 = 0
- 
+
 while count2 <= 5:
-    
+
     n = int(input("intruducir un numero natural"))
-    
+
     def fibonacci(n):
-        
+
         if n == 0:
-            
+
             return 0
-        
-        elif  n == 1:
-        
+
+        elif n == 1:
+
             return 1
-        
+
         else:
-           
-           return fibonacci(n-1)+fibonacci(n-2)
-        
-    
-    print("el termino en la posicion", n , "de Fibonacci es", fibonacci(n))
-    
-    count2 += 1 
-    
+
+            return fibonacci(n-1)+fibonacci(n-2)
+
+    print("el termino en la posicion", n, "de Fibonacci es", fibonacci(n))
+
+    count2 += 1
